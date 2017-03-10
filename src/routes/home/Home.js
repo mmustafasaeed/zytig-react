bac40125b35bd8e9f4b032ea32efab868f30ca6f
Sara Eriksson <Sara@n151-p232.kthopen.kth.se>
2017-03-10 15:03:07 +0100

    some changes

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -68,11 +68,11 @@ class Home extends Component {
     return (
       <div className="animate">
         <div className="row">
-          <div className="col-md-4 col-lg-3">
+          <div className="col-md-12 col-lg-12">
             <div className={s.homeStats}>
-
+              <div className="col-md-3 col-lg-3">
               <Stats
-                icon="cloud-upload"
+                icon="tint"
                 text={
                   <FormattedMessage
                     id="stat1"
@@ -84,6 +84,9 @@ class Home extends Component {
                 value="9"
                 progressValue={90}
               />
+
+              </div>
+              <div className="col-md-3 col-lg-3">
               <Stats
                 icon="heartbeat"
                 text={
@@ -97,8 +100,11 @@ class Home extends Component {
                 value="94%"
                 progressValue={94}
               />
+              </div>
+
+              <div className="col-md-3 col-lg-3">
               <Stats
-                icon="flag"
+                icon="person-walking"
 
                 text={
                   <FormattedMessage
@@ -111,11 +117,31 @@ class Home extends Component {
                 value="12,351"
                 progressValue={72}
               />
+              </div>
+
+              <div className="col-md-3 col-lg-3">
+              <Stats
+                icon="flag"
+
+                text={
+                  <FormattedMessage
+                    id="stat4"
+                    defaultMessage="Posts flagged"
+                  />
+                }
+                bgClass="danger"
+                link="/dashboard/inbox"
+                value="12,351"
+                progressValue={72}
+              />
+              </div>
 
             </div>
           </div>
+        </div>
 
-          <div className="col-md-5 col-lg-6">
+        <div className="row">
+          <div className="col-md-11 col-lg-12">
             <div className="map-container box padder">
               <div className="panel">
                 <div className="panel-body">
@@ -154,7 +180,30 @@ class Home extends Component {
               </div>
             </div>
           </div>
+        </div>
 
+        <div className="row">
+          <div className="col-md-12 col-lg-12">
+            <div className={`map-container box padder ${s.powerSupplyChart}`}>
+              <Panel header={<span>Power Supply Waves</span>}>
+                <div>
+                  <ResponsiveContainer width="100%" height={275}>
+                    <LineChart data={lineChartData}>
+                      <CartesianGrid stroke="#ccc" />
+                      <XAxis />
+                      <YAxis />
+                      <Tooltip />
+                      <LineRechart type="monotone" dataKey="sine" stroke="#8884d8" />
+                      <LineRechart type="monotone" dataKey="cosine" stroke="#82ca9d" />
+                    </LineChart>
+                  </ResponsiveContainer>
+                </div>
+              </Panel>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
           <div className="col-md-3 col-lg-3">
             <div className="row">
               <div className="col-sm-12">
@@ -168,6 +217,11 @@ class Home extends Component {
                 </div>
               </div>
             </div>
+
+
+
+
+
             <div className={'row'}>
               <div className="col-sm-12 text-left">
                 <div className={`${s.bottomRightChart}`}>
@@ -195,33 +249,13 @@ class Home extends Component {
               </div>
             </div>
           </div>
-
         </div>
+
 
         <div className="row">
 
-
-          <div className="col-md-8 col-lg-8">
-            <div className={`map-container box padder ${s.powerSupplyChart}`}>
-              <Panel header={<span>Power Supply Waves</span>}>
-                <div>
-                  <ResponsiveContainer width="100%" height={275}>
-                    <LineChart data={lineChartData}>
-                      <CartesianGrid stroke="#ccc" />
-                      <XAxis />
-                      <YAxis />
-                      <Tooltip />
-                      <LineRechart type="monotone" dataKey="sine" stroke="#8884d8" />
-                      <LineRechart type="monotone" dataKey="cosine" stroke="#82ca9d" />
-                    </LineChart>
-                  </ResponsiveContainer>
-                </div>
-              </Panel>
-            </div>
-          </div>
-
           <div className="col-md-4 col-lg-4">
-            <TodoList />
+           <TodoList />
           </div>
         </div>
       </div>
